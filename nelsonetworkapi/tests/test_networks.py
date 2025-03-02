8a4f38e0830b38634159c254642ed81474b9bd7a
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2025-03-02 09:49:34 -0600

    Test to DELETE a Network working

--- nelsonetworkapi/tests/test_networks.py
+++ nelsonetworkapi/tests/test_networks.py
@@ -106,7 +106,7 @@ class NetworkTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)  # Ensure update is successful
 
-        # Refresh the tour object to reflect any changes in the database
+        # Refresh the network object to reflect any changes in the database
         network.refresh_from_db()
 
         # Assert that the updated values match
@@ -118,3 +118,19 @@ class NetworkTests(APITestCase):
         self.assertEqual(updated_network["location"], network.location) 
         self.assertEqual(updated_network["user_id"], network.user_id)
         self.assertEqual(updated_network["device_id"], network.device_id.pk) 
+
+# Test to DELETE a Network
+# run this command: python manage.py test nelsonetworkapi.tests.test_networks
+
+    def test_delete_network(self):
+        """Test delete tour
+        """
+        network = Network.objects.first()
+        
+        url = f'/networks/{network.network_id}'
+        response = self.client.delete(url)
+        
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        
+        response =self.client.get(url)
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
