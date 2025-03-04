7c85386c9f3f8483426f580123ecff5eda18e941
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2025-03-03 22:32:45 -0600

    Test to GET a SINGLE documentation works

--- nelsonetworkapi/tests/test_documentations.py
+++ nelsonetworkapi/tests/test_documentations.py
@@ -17,6 +17,9 @@ class DocumentationAPITests(APITestCase):
             configuration="interface eth0\n ip address 192.168.1.1"
         )
 
+# Test to create a New Documentation
+# run this command: python manage.py test nelsonetworkapi.tests.test_documentations
+
     def test_create_documentation(self):
         """Test creating a new Documentation instance via API."""
         url = reverse("documentation-list")  # Assumes basename='documentation'
@@ -28,3 +31,13 @@ class DocumentationAPITests(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["device_type"], "Switch")
+
+# Test to GET a SINGLE Documentation
+# run this command: python manage.py test nelsonetworkapi.tests.test_documentations
+
+    def test_get_documentation(self):
+        """Test retrieving a single Documentation instance."""
+        url = reverse("documentation-detail", kwargs={"pk": self.documentation.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["device_type"], "Router")
